89e5beed302af508b2de0ab880cb8ce019fc0e81
Jack Donofrio <john.a.donofrio.25@dartmouth.edu>
2023-03-20 18:52:42 -0400

    cleaned up some code

--- dungeons.c
+++ dungeons.c
@@ -6,17 +6,32 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <ncurses.h>
 
 // note - hard-coded dims based on input file
 #define MAP_WIDTH 32
 #define MAP_HEIGHT 16
+#define PLAYER_SYMBOL '@'
+#define OPEN_SPACE '.'
+#define HALLWAY '#'
+
+typedef struct player
+{
+    int row;
+    int column;
+    uint health; // integer from 0 to 100
+} player;
 
 char get_map_char(int row, int col, char* map);
 char* load_map(char* filename);
 void setup_ncurses();
 void write_map_curse(char* map);
 void curse_put(int row, int col, char c, int color);
+void handle_keypress(player* player_ptr, char key, char* map);
+void handle_walk_key(player* player_ptr, char* map, int row_change, int col_change);
+bool can_step(char* map, int row, int column);
+player* player_init(int start_row, int start_col);
 
 
 int main()
@@ -25,54 +40,78 @@ int main()
     setup_ncurses();
     write_map_curse(map);
 
-    int player_row = 2;
-    int player_col = 2;
-    char map_char;
+    int start_row = 2;
+    int start_col = 2;
+    player* player_ptr = player_init(start_row, start_col);
 
-    curse_put(player_row, player_col, '@', 1);
-
-    char c = '\0';
-    while (c != 'q') {
-        c = getch();
-        switch (c) {
-            case 'q':
-                break;
-            case 'w':
-                map_char = get_map_char(player_row - 1, player_col, map);
-                if (map_char == '.') {
-                    player_row--;
-                }
-                break;
-            case 'a':
-                map_char = get_map_char(player_row, player_col - 1, map);
-                if (map_char == '.') {
-                    player_col--;
-                }
-                break;
-            case 's':
-                map_char = get_map_char(player_row + 1, player_col, map);
-                if (map_char == '.') {
-                    player_row++;
-                }
-                break;
-            case 'd':
-                map_char = get_map_char(player_row, player_col + 1, map);
-                if (map_char == '.') {
-                    player_col++;
-                }
-                break;
-        }
-        
-        // update map 
+    char key = '\0';
+    while ((key = getch()) != 'q') {
+        handle_keypress(player_ptr, key, map);        
+        // update map and player location
         write_map_curse(map);    
-        curse_put(player_row, player_col, '@', 1);
+        curse_put(player_ptr->row, player_ptr->column, PLAYER_SYMBOL, 2);
         // refresh();
     }
     free(map);
+    free(player_ptr);
     endwin();
     return 0;
 }
 
+void handle_keypress(player* player_ptr, char key, char* map)
+{
+    char map_char;
+    switch (key) {
+        case 'q':
+            break;
+        case 'w':
+            handle_walk_key(player_ptr, map, -1, 0);
+            break;
+        case 'a':
+            handle_walk_key(player_ptr, map, 0, -1);
+            break;
+        case 's':
+            handle_walk_key(player_ptr, map, 1, 0);
+            break;
+        case 'd':
+            handle_walk_key(player_ptr, map, 0, 1);
+            break;
+        }
+}
+
+void handle_walk_key(player* player_ptr, char* map, int row_change, int col_change)
+{
+    int new_row = player_ptr->row + row_change;
+    int new_col = player_ptr->column + col_change;
+    if (can_step(map, new_row, new_col)) {
+        player_ptr->row = new_row;
+        player_ptr->column = new_col;
+    }
+}
+
+player* player_init(int start_row, int start_column)
+{
+    player* p = malloc(sizeof(player));
+    if (p == NULL) {
+        fprintf(stderr, "error: unable to allocate player data\n");
+        exit(1);
+        return NULL;
+    }
+
+    p->row = start_row;
+    p->column = start_column;
+    p->health = 100; // starting health - make macro later later
+
+    // move to start location
+    curse_put(start_row, start_column, PLAYER_SYMBOL, 2);
+    return p;
+}
+
+bool can_step(char* map, int row, int column)
+{
+    char map_char = get_map_char(row, column, map);
+    return map_char == OPEN_SPACE || map_char == HALLWAY;
+}
 
 char get_map_char(int row, int col, char* map)
 {
